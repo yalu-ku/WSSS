ee083dd0d57d86500bbf9d6c1ca041a07033813f
herbwood <herbwood27@gmail.com>
2022-02-04 18:54:57 +0900

    added args.network

--- cls/train_cls.sh
+++ cls/train_cls.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-EXP_NAME=deformv2_baseline
+EXP_NAME=vgg_deform_momentum
 
-CUDA_VISIBLE_DEVICES=2 python scripts/train_cls.py \
+CUDA_VISIBLE_DEVICES=1 python scripts/train_cls.py \
     --img_dir=/HDD/dataset/VOC2012/ \
     --train_list=/home/junehyoung/code/wsss_baseline/voc2012_list/train_aug_cls.txt \
     --test_list=/home/junehyoung/code/wsss_baseline/voc2012_list/train_cls.txt \
@@ -10,4 +10,6 @@ CUDA_VISIBLE_DEVICES=2 python scripts/train_cls.py \
     --decay_points='5,10' \
     --save_folder=checkpoints/${EXP_NAME} \
     --show_interval=50 \
-    --wandb_name=${EXP_NAME} 
+    --batch_size 5 \
+    --wandb_name=${EXP_NAME} \
+    --network=models.${EXP_NAME}
