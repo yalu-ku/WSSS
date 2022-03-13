4267d73ad07f11dac44e3adf7c3822f34f708555
herbwood <herbwood27@gmail.com>
2022-03-13 17:41:42 +0900

    revised train, test list path

--- cls/train_cls.sh
+++ cls/train_cls.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-EXP_NAME=vgg_deform_momentum
+EXP_NAME=offset_smooth_b03d07
 
 CUDA_VISIBLE_DEVICES=1 python scripts/train_cls.py \
     --img_dir=/HDD/dataset/VOC2012/ \
-    --train_list=/home/junehyoung/code/wsss_baseline/voc2012_list/train_aug_cls.txt \
-    --test_list=/home/junehyoung/code/wsss_baseline/voc2012_list/train_cls.txt \
+    --train_list=/home/junehyoung/code/wsss_baseline2/metadata/voc12/train_aug_cls.txt \
+    --test_list=/home/junehyoung/code/wsss_baseline2/metadata/voc12/train_cls.txt \
     --lr=0.001 \
     --epoch=15 \
     --decay_points='5,10' \
     --save_folder=checkpoints/${EXP_NAME} \
     --show_interval=50 \
     --batch_size 5 \
-    --wandb_name=${EXP_NAME} \
-    --network=models.${EXP_NAME}
+    --wandb_name=${EXP_NAME}
