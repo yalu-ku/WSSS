1d71fb2ebe29b40fdbe92598273d3bf693e60d28
herbwood <herbwood27@gmail.com>
2022-03-13 21:08:04 +0900

    changed train,test list

--- cls/train_refine.sh
+++ cls/train_refine.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-CUDA_VISIBLE_DEVICES=1,2 python scripts/train_refine.py \
+CUDA_VISIBLE_DEVICES=1,3 python scripts/train_refine.py \
     --img_dir=/HDD/dataset/VOC2012/ \
-    --train_list=/home/junehyoung/code/wsss_baseline/metadata/voc12/train_aug_cls.txt \
-    --test_list=/home/junehyoung/code/wsss_baseline/metadata/voc12/train_cls.txt \
+    --train_list=/home/junehyoung/code/wsss_baseline2/metadata/voc12/train_aug_cls.txt \
+    --test_list=/home/junehyoung/code/wsss_baseline2/metadata/voc12/train_cls.txt \
     --lr=0.0001 \
     --epoch=30 \
     --decay_points='10,20' \
-    --save_folder=checkpoints/refinement_learning/deform_relu_init \
+    --save_folder=checkpoints/refinement_learning/offset_smooth_b03d05_fgbg \
     --show_interval=50 \
-    --wandb_name refinement_learning_deform_relu_init
+    --shuffle_val \
+    --wandb_name refinement_learning_offset_smooth_b03d05_fgbg
