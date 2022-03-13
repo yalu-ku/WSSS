fa2804a550ca5f65830168c210572bd14c8b7a21
herbwood <herbwood27@gmail.com>
2022-03-13 21:07:30 +0900

    added locmap_dir argparse

--- cls/localization_map_gen.sh
+++ cls/localization_map_gen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-CUDA_VISIBLE_DEVICES=1 python scripts/localization_map_gen.py \
+CUDA_VISIBLE_DEVICES=3 python scripts/localization_map_gen.py \
     --img_dir=/HDD/dataset/VOC2012/ \
-    --checkpoint=/home/junehyoung/code/wsss_baseline/cls/checkpoints/vgg_deform_offsetscaler_relu_init/best.pth
+    --checkpoint=/home/junehyoung/code/wsss_baseline2/cls/checkpoints/offset_smooth_b03d05/best.pth \
+    --locmap_dir=localization_map_offset_smooth_b03d05
